a3d977d168f3de9991b56070a92919d6e5dbc2cc
xiaoji121 <dong.fly.ming@gmail.com>
2014-12-02 23:21:59 +0800

    coding

---
+++ app/static/js/common.js
@@ -0,0 +1,37 @@
+/**
+ * Created by dongming on 14-12-2.
+ */
+
+var app = angular.module('myFbo.common', ['firebase']);
+
+app.factory("Auth", ["$firebaseAuth", function($firebaseAuth) {
+    var ref = new Firebase("https://fbo.firebaseio.com/");
+    return $firebaseAuth(ref);
+}]);
+
+app.controller("AuthCtrl", ["$scope", "Auth", '$location', function($scope, Auth, $location) {
+    $scope.userInfo = Auth.$getAuth();
+
+    $scope.isLogin = $scope.userInfo ? true : false;
+
+    $scope.login = function() {
+        return Auth.$authWithOAuthPopup('github');
+    };
+
+    $scope.logout = function() {
+        if ($scope.isLogin) {
+            return Auth.$unauth();
+        }
+
+        return;
+    };
+
+    Auth.$onAuth(function(authData) {
+        if (authData) {
+            $location.path('/home');
+        } else {
+            $location.path('/');
+        }
+    });
+
+}]);
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -2,79 +2,13 @@
  * Created by dongming on 14-12-1.
  */
 
-var app = angular.module("sampleApp", ["firebase", "ngRoute"]);
-// let's create a re-usable factory that generates the $firebaseAuth instance
-app.factory("Auth", ["$firebaseAuth", function($firebaseAuth) {
-    var ref = new Firebase("https://fbo.firebaseio.com/");
-    return $firebaseAuth(ref);
-}]);
-// and use it in our controller
-app.controller("SampleCtrl", ["$scope", "Auth", function($scope, Auth) {
-    $scope.auth = Auth;
-    $scope.user = $scope.auth.$getAuth();
+var app = angular.module('myFbo', [
+    'myFbo.common',
+    'myFbo.router'
+]);
 
-    $scope.logOut = function() {
-        if ($scope.user) {
-            $scope.auth.$unauth()
-        }
-    };
-
-    $scope.auth.$onAuth(function(authData) {
-        if (authData) {
-            console.log("登陆成功，用户名", authData.github.username);
-        } else {
-            console.log("退出成功");
-        }
-    });
-
-}]);
-
-app.run(["$rootScope", "$location", function($rootScope, $location) {
-    $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
-        // We can catch the error thrown when the $requireAuth promise is rejected
-        // and redirect the user back to the home page
-        if (error === "AUTH_REQUIRED") {
-            $location.path("/home");
-        }
-    });
-}]);
-
-app.config(["$routeProvider", function($routeProvider) {
-    $routeProvider.when("/home", {
-        // the rest is the same for ui-router and ngRoute...
-        controller: "HomeCtrl",
-        templateUrl: "views/home.html",
-        resolve: {
-            // controller will not be loaded until $waitForAuth resolves
-            // Auth refers to our $firebaseAuth wrapper in the example above
-            "currentAuth": ["Auth", function(Auth) {
-                // $waitForAuth returns a promise so the resolve waits for it to complete
-                return Auth.$waitForAuth();
-            }]
-        }
-    }).when("/account", {
-        // the rest is the same for ui-router and ngRoute...
-        controller: "AccountCtrl",
-        templateUrl: "views/account.html",
-        resolve: {
-            // controller will not be loaded until $requireAuth resolves
-            // Auth refers to our $firebaseAuth wrapper in the example above
-            "currentAuth": ["Auth", function(Auth) {
-                // $requireAuth returns a promise so the resolve waits for it to complete
-                // If the promise is rejected, it will throw a $stateChangeError (see above)
-                return Auth.$requireAuth();
-            }]
-        }
-    });
-}]);
-
-app.controller("HomeCtrl", ["currentAuth", function(currentAuth) {
-    // currentAuth (provided by resolve) will contain the
-    // authenticated user or null if not logged in
-    console.log(currentAuth);
-}]);
-
-app.controller("AccountCtrl", ["currentAuth", function(currentAuth) {
-    // currentAuth (provided by resolve) will contain the
-    // authenticated user or null if not logged in
+app.controller('IndexCtrl', ['$location', 'Auth', function($location, Auth) {
+    if (Auth.$getAuth()) {
+       $location.path('/home');
+    }
 }]);
